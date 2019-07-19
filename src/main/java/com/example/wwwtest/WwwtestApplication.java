818997e6b27e685fa35356d69cbdb38bcd381bb7
Mrk-ing <95780617@qq.com>
2019-07-19 10:31:51 +0800

    测试

---
+++ src/main/java/com/example/wwwtest/WwwtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wwwtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwtestApplication.class, args);
+    }
+
+}
